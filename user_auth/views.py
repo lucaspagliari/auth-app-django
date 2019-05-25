7aad6f613edf27f7253349b055a48f0144c1151c
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-05-25 16:16:31 -0300

    New fields
    New Form Fields added, email, name and lastname

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from user_auth.forms import RegistrationForm
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            redirect('profile')
+    else:
+        form = RegistrationForm()
+
+    args = {'form' : form}
+    return render(request, 'registration/register.html', args)
 
 
 def index(request):
     return render(request, 'user_auth/index.html')
 
+def profile(request):
+    return render(request, 'user_auth/profile.html')
+'''
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -23,5 +41,5 @@ def register(request):
 
 def profile(request):
     return render(request, 'user_auth/profile.html')
-
+'''
 
