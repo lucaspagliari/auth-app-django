560d739e8e525b25e92c71dec92e04224417f6b0
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-05-26 12:26:47 -0300

    Interface

--- DEV_0419/settings.py
+++ DEV_0419/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
