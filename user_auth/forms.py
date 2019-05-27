e5968df92be1953e23bccb3eed477e0ef4f41ff0
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-05-26 23:36:18 -0300

    interface index ready

--- user_auth/forms.py
+++ user_auth/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -17,7 +18,7 @@ class RegistrationForm(UserCreationForm):
         )
 
     def save(self, commit=True):
-        user = super(RegistrationForm,self).save(commit=False)
+        user = super(RegistrationForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
--- user_auth/views.py
+++ user_auth/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import authenticate, login
 from user_auth.forms import RegistrationForm
 
 
-
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -14,15 +13,18 @@ def register(request):
     else:
         form = RegistrationForm()
 
-    args = {'form' : form}
+    args = {'form': form}
     return render(request, 'registration/register.html', args)
 
 
 def index(request):
     return render(request, 'user_auth/index.html')
 
+
 def profile(request):
     return render(request, 'user_auth/profile.html')
+
+
 '''
 def register(request):
     if request.method == 'POST':
@@ -42,4 +44,3 @@ def register(request):
 def profile(request):
     return render(request, 'user_auth/profile.html')
 '''
-
